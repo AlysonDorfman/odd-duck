9a5abf81bb57d5762657dc0bfe0a9b4ed2a754b7
Alyson Dorfman <alyson@radiusclinic.com>
2024-01-08 15:52:48 -0800

    updated

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # odd-duck
+
+What do we need?
+
+Constructor
+
+  Store our goat info:
+    - votes
+    - Names
+    - times it has been shown
+    - image
+    - 
+
+Executable Code
+
+Notes
+
+- Event Listener
+  - Click
+    - increads the votes for teh goat (store the votes)
+  - Counter 
+  - Button for viewing results
+    - Display votes
+    - Event Listener (kicks in after voting rounds complete)
+  - Conditional logic to make sure images are unique
+  - Logic to make sure images are random
+  - Dom references
+    - Button 
+    - images
+
---
+++ app.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// *** GLOBAL VARIABLES *** //
+let votingRounds = 25;
+let goatArray = []; // to hold all the objects, may not need?
+
+// *** DOM WINDOWS *** //
+let imgContainer = document.getElementById('img-container');
+let imgOne = document.getElementById(img-one);
+let imgTwo = document.getElementById(img-two);
+let resultBtn = document.getElementById('show-results-btn');
+let resultsList = document.getElementById('results-container');
+
+// *** CONSTUCTOR FUNTION *** //
+function Goat(name, image) {
+  this.name = name;
+  this.image = image;
+  this.votes = 0;
+  this.views = 0
+}
+
+
+// *** HELPER FUNTIONS / UTILITES *** //
+
+
+// *** EVENT HANDLERS *** //
+
+
+// *** EXECUTABLE CODE *** //
